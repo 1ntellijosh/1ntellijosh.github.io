0be6e29316a38e535c94e9e3242379df40013580
Josh <admin@pop-os.localdomain>
2025-12-07 21:05:03 -0700

    game entry point`

--- js/EntryPoints/StarFighterEntryPoint.js
+++ js/EntryPoints/StarFighterEntryPoint.js
@@ -2,7 +2,7 @@
  * Main entry point for the StarFighter game
  * Initializes the game when DOM is ready
  */
-import Game from './Game.js';
+import Game from '../Game.js';
 
 export default function StarFighterEntryPoint() {
   // Main entry point for the game
